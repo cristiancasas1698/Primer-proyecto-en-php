12d27ac2c30536d6a5f9dc17bb6df942b90c1016
cristian-casas <cristiancasas1698@gmail.com>
2021-01-18 20:22:14 -0500

    agregando composer al proyecto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a00a5379905f49348a6e479d6e83ce4cd3ca144f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a00a5379905f49348a6e479d6e83ce4cd3ca144f',
+    ),
+  ),
+);
